b20eb945f1b2193797414e53cd26ddeb2a271cd5
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-01-22 15:39:59 -0800

    Task 100 modified

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+from __future__ import print_function
 import sys
 
-def safe_print_integer_err(value):
+
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
+        res = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return False
+        return None
+    else:
+        return res
