1ada7e3ac527634d6469d4c25bd2acc73d81b277
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-01-23 14:18:45 -0800

    Task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,10 +4,10 @@ import math
 class MagicClass:
     def __init__(self, radius):
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number') 
         self.__radius = radius
-    
+            
     def area(self):
         return math.pi * self.__radius ** 2
 
