d1a59c578fbd28a2240d77af338e5de4c5d783aa
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:27:34 -0800

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python3
+"""
+Module: 3-to_json_string
+Contains function: to_json_string
+"""
+
 import json
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)"""
+    """
+    Returns the JSON representation of an object (string)
+    Args:
+        my_obj: Object to be serialized to JSON
+    Returns:
+        str: JSON representation of the object
+    """
     return json.dumps(my_obj)
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3]
+    s_my_list = to_json_string(my_list)
+    print(s_my_list)
+    print(type(s_my_list))
+
+    my_dict = { 
+            'id': 12,
+            'name': "John",
+            'places': [ "San Francisco", "Tokyo" ],
+            'is_active': True,
+            'info': {
+                'age': 36,
+                'average': 3.14
+                }
+            }
+    s_my_dict = to_json_string(my_dict)
+    print(s_my_dict)
+    print(type(s_my_dict))
+
+    try:
+        my_set = { 132, 3 }
+        s_my_set = to_json_string(my_set)
+        print(s_my_set)
+        print(type(s_my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
