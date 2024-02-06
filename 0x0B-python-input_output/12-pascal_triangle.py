edf6b37cd7d5a907934a39cb48405aa77cc942e6
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:46:54 -0800

    Task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Module: 12-pascal_triangle
+Contains function: pascal_triangle
+"""
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n.
+
+    Args:
+        n (int): The number of rows in the Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
     if n <= 0:
         return []
 
@@ -8,9 +21,17 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
+        prev_row = triangle[i - 1]
+
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(prev_row[j - 1] + prev_row[j])
+
         row.append(1)
         triangle.append(row)
 
     return triangle
+
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    for row in triangle:
+        print(row)
