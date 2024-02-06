fa2360dfa5aa93180594d865ce13506c94766a36
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:22:43 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+Module: 1-write_file
+Contains function: write_file
+"""
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF-8) and returns the number of characters written"""
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written
+    Args:
+        filename (str): Name of the file to write to
+        text (str): Text to write to the file
+    Returns:
+        int: Number of characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
+
+if __name__ == "__main__":
+    write_file("my_first_file.txt", "This School is so cool!\n")
+
