685ecb758731809cd9d0e90876b442da099a820a
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:26:13 -0800

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+Module: 2-append_write
+Contains function: append_write
+"""
+
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF-8) and returns the number of characters added"""
-    with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+    """
+    Appends a string at the end of a text file (UTF8) and returns the number of characters added
+    Args:
+        filename (str): Name of the file to append to
+        text (str): Text to append to the file
+    Returns:
+        int: Number of characters added
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
+
+if __name__ == "__main__":
+    append_write("file_append.txt", "This School is so cool!\n")
