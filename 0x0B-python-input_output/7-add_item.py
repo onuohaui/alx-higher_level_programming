bf561a7712e5b41ab4f24b705572dd599a041060
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:39:29 -0800

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,18 @@ Adds all arguments to a Python list and saves them to a file
 """
 
 import sys
-from os import path
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
+import os
 
 def main():
     """Main function of the script"""
     filename = "add_item.json"
     items = []
 
-    # Check if the file exists and load its content
-    if path.exists(filename):
+    # Check if the file exists
+    if os.path.exists(filename):
+        # Load existing items from file
         items = load_from_json_file(filename)
 
     # Add arguments to the list
