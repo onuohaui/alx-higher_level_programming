9da0e5b387b1337d45303a32f874ef60b5e20f13
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:40:55 -0800

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Module: 8-class_to_json
+Contains function: class_to_json
+"""
+
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object"""
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary representing the attributes of the object.
+    """
     return obj.__dict__
