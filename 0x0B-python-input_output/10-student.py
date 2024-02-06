e05b3dd45252fd79d09f6c4a755877bb4f30a35d
Onuoha UI <ifechinyereonuoha@gmail.com>
2024-02-06 07:43:17 -0800

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python3
+"""
+Module: 10-student
+Contains class: Student
+"""
+
 class Student:
-    """Student class"""
+    """
+    Represents a student.
+    """
+
     def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance"""
-        if isinstance(attrs, list) and all(isinstance(el, str) for el in attrs):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional): A list of strings representing attributes to be included.
+                If provided, only these attributes will be retrieved.
+
+        Returns:
+            dict: A dictionary containing the specified attributes of the student.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
